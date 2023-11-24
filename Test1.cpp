b0f2ce353fbdf71ae077d70c935ae1afbfbd6dbe
Patt126 <145294744+Patt126@users.noreply.github.com>
2023-11-24 15:44:13 +0100

    Update Test1.cpp
    CHECK HOW ENERGY IS EVALUATED
    - magnetisation evaluation
    - write data on output txt file
    - a function for exact energy evaluation if needed
    -

--- Test1.cpp
+++ Test1.cpp
@@ -1,18 +1,59 @@
-#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <vector>
 #include <iostream>
+#include<fstream>
+
 
 
 #define L 100
 #define N (L*L)
-#define T 2.4
+#define T 0.01
 #define beta (1/T)
 #define J 1.00
+#define IT 6*1e7 //number of iterations
 
+void print_lattice(std::vector < std::vector<int> >& matrix) {
 
-int flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob) {
+    int i, j;
+    for (i = 0; i < L; i++) {
+        std::cout << std::endl;
+        for (j = 0; j < L; j++) {
+            if (matrix[i][j] == -1) {
+                std::cout << "o" << " ";
+            } else {
+                std::cout << "x" << " ";
+
+            }
+        }
+    }
+}
+
+//if needed function to correctly evaluate energy
+float evaluate(std::vector < std::vector<int> >& matrix) {
+    int sum=0;
+    for (int i=0;i<L;i++){
+        for (int j=0; j<L;j++){
+
+            if (i != 0) {
+                sum += matrix[i - 1][j]*matrix[i][j]*2;
+            }
+            if (j != 0) {
+                sum += matrix[i][j-1]*matrix[i][j]*2;
+            }
+
+            if(i == L - 1) {
+                sum += matrix[0][j]*matrix[i][j]*2; //times 2 two count also contribute where i = 0
+            }
+            if(j == L - 1) {
+                sum += matrix[i][0]*matrix[i][j]*2;
+            }
+        }
+        }
+    return -J*sum;
+    }
+
+void flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob,float& energy, int& M) {
 
 	int r, c;
 	r = rand() % L;
@@ -46,7 +87,6 @@ int flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob) {
 	}
     int delta = 2*sum*matrix[r][c];
 	if (delta <= 0) {
-        //std::cout<<"neg ";
 		matrix[r][c] = -matrix[r][c];
 	}
 	else if (delta == 4) {
@@ -55,7 +95,7 @@ int flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob) {
 			matrix[r][c] = -matrix[r][c];
 		}
         else{
-            return 0;
+            return;
         }
 	}
 	else if (delta==8){
@@ -64,66 +104,66 @@ int flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob) {
 			matrix[r][c] = -matrix[r][c];
 		}
         else{
-            return 0;
+            return;
         }
 	}
 
-	return delta*J;
+	energy += 2*delta*J;
+    M += 2*matrix[r][c];
+
 }
 
-float initialize_lattice(std::vector < std::vector<int> >& matrix) {
-	int k,m=0;
-    int energy = 0;
+void initialize_lattice(std::vector < std::vector<int> >& matrix, float& energy, int& M) {
+	int k;
 	int size = L;
+    int sum = 0;
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			k = rand() % 2;
 			if (k == 0) {
 				matrix[i][j] = -1;
+                M -= 1;
 			}
 			else {
 				matrix[i][j] = 1;
+                M += 1;
 			}
             //energy contibute
             if (i != 0) {
-                energy += matrix[i - 1][j]*matrix[i][j]*2;
+                sum += matrix[i - 1][j]*matrix[i][j]*2;
             }
             if (j != 0) {
-                energy += matrix[i][j-1]*matrix[i][j]*2;
+                sum += matrix[i][j-1]*matrix[i][j]*2;
             }
 
             if(i == L - 1) {
-                energy += matrix[0][j]*matrix[i][j]*2; //times 2 two count also contribute where i = 0
+                sum += matrix[0][j]*matrix[i][j]*2; //times 2 two count also contribute where i = 0
             }
             if(j == L - 1) {
-                energy += matrix[i][0]*matrix[i][j]*2;
+                sum += matrix[i][0]*matrix[i][j]*2;
             }
             }
 		}
-    std::cout<<std::endl<<energy<<' ';
-    return -J*energy;
+    energy+= -J*sum;
 	}
 
 
 
-void print_lattice(std::vector < std::vector<int> >& matrix) {
 
-    int i, j;
-    for (i = 0; i < L; i++) {
-        std::cout << std::endl;
-        for (j = 0; j < L; j++) {
-            if (matrix[i][j] == -1) {
-                std::cout << "o" << " ";
-            } else {
-                std::cout << "x" << " ";
 
-            }
+
+int write_file(std::vector<float>& energy_vec, std::vector<float>& m, std::vector<int>& t){
+    std::ofstream myfile ("data.txt");
+    if (myfile.is_open())
+    {
+        for(int i = 0; i < t.size(); i ++){
+            myfile << energy_vec[i] << " "<<m[i]<<' '<<t[i]<<'\n';
         }
+        myfile.close();
     }
+    else std::cout << "Unable to open file";
+    return 0;
 }
-
-
-
 
 
 
@@ -131,30 +171,39 @@ int main() {
 
 	using namespace std;
 	vector<vector<int> > lattice(L, vector<int>(L));
-	float energy = initialize_lattice(lattice);
+	float energy = 0;
+    int M =0;
+    initialize_lattice(lattice,energy,M);
+
     vector<float> prob(2);
 	prob[0] = exp(-4 * J*beta);
     prob[1] = exp(-8 * J*beta);
+
 	vector<float> energy_vec(1);
     energy_vec[0] =  energy;
+
+    vector<float> m(1);
+    m[0] = (float)M/N;
+
 	vector<int> t_axis(1);
     t_axis[0] = 0;
-
-    cout << energy << endl;
-	for (int i = 0; i < 6*1e7; i++){
-		energy += flip(lattice, prob);
+    print_lattice(lattice);
+    cout<<endl;
+	for (int i = 1; i < IT; i++){
+		flip(lattice, prob, energy, M);
         if (i % N == 0) {
-            // aggiornare l'energia ogni N step (definizione di tempo nel metropolis come da libro)
-           cout << energy << endl;
-            energy_vec.push_back(energy_vec.back() + energy);
-            t_axis.push_back(i/N);
+            // aggiornare l'energia e la magnetizzazione ogni N step (definizione di tempo nel metropolis come da libro)
+            m.push_back((float) M / N); // magnetizzazione media per sito
+            energy_vec.push_back(energy);
+            t_axis.push_back(i / N);
 
-		}
+        }
 	}
 
-
-
+    write_file(energy_vec,m,t_axis);
+    print_lattice(lattice);
 
 
 	return 0;
 }
+
