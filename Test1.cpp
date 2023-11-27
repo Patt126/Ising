8f1592749ae30012504adf8dc605119beb213b42
Luigi Pagani <127208537+LuigiPagani@users.noreply.github.com>
2023-11-27 16:43:05 +0100

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include<fstream>
+#include<tuple>
 
 
 
@@ -19,7 +20,8 @@ void print_lattice(std::vector < std::vector<int> >& matrix) {
         for (j = 0; j < L; j++) {
             if (matrix[i][j] == -1) {
                 std::cout << "o" << " ";
-            } else {
+            }
+            else {
                 std::cout << "x" << " ";
 
             }
@@ -29,133 +31,130 @@ void print_lattice(std::vector < std::vector<int> >& matrix) {
 
 //if needed function to correctly evaluate energy
 float evaluate(std::vector < std::vector<int> >& matrix) {
-    int sum=0;
-    for (int i=0;i<L;i++){
-        for (int j=0; j<L;j++){
+    int sum = 0;
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < L; j++) {
 
             if (i != 0) {
-                sum += matrix[i - 1][j]*matrix[i][j]*2;
+                sum += matrix[i - 1][j] * matrix[i][j] * 2;
             }
             if (j != 0) {
-                sum += matrix[i][j-1]*matrix[i][j]*2;
+                sum += matrix[i][j - 1] * matrix[i][j] * 2;
             }
 
-            if(i == L - 1) {
-                sum += matrix[0][j]*matrix[i][j]*2; //times 2 two count also contribute where i = 0
+            if (i == L - 1) {
+                sum += matrix[0][j] * matrix[i][j] * 2; //times 2 two count also contribute where i = 0
             }
-            if(j == L - 1) {
-                sum += matrix[i][0]*matrix[i][j]*2;
+            if (j == L - 1) {
+                sum += matrix[i][0] * matrix[i][j] * 2;
             }
         }
-        }
-    return -J*sum;
+    }
+    return -J * sum;
+}
+
+void flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob, float& energy, int& M,int r, int c) {
+
+    int sum = 0;
+
+    if (r == 0) {
+        sum += matrix[L - 1][c];
+    }
+    else {
+        sum += matrix[r - 1][c];
+    }
+    if (c == 0) {
+        sum += matrix[r][L - 1];
+    }
+    else {
+        sum += matrix[r][c - 1];
     }
 
-void flip(std::vector < std::vector<int> >& matrix, std::vector<float>& prob,float& energy, int& M) {
-
-	int r, c;
-	r = rand() % L;
-	c = rand() % L;
-	int sum = 0;
-
-	if (r == 0) {
-		sum += matrix[L - 1][c];
-	}
-	else {
-		sum += matrix[r - 1][c];
-	}
-	if (c == 0) {
-		sum += matrix[r][L - 1];
-	}
-	else {
-		sum += matrix[r][c - 1];
-	}
-
-	if (r == L - 1) {
-		sum += matrix[0][c];
-	}
-	else {
-		sum += matrix[r + 1][c];
-	}
-	if (c == L - 1) {
-		sum += matrix[r][0];
-	}
-	else {
-		sum += matrix[r][c + 1];
-	}
-    int delta = 2*sum*matrix[r][c];
-	if (delta <= 0) {
-		matrix[r][c] = -matrix[r][c];
-	}
-	else if (delta == 4) {
-        float rnd = (rand() % 10000)/1e4;
-		if (rnd < prob[0] ){
-			matrix[r][c] = -matrix[r][c];
-		}
-        else{
+    if (r == L - 1) {
+        sum += matrix[0][c];
+    }
+    else {
+        sum += matrix[r + 1][c];
+    }
+    if (c == L - 1) {
+        sum += matrix[r][0];
+    }
+    else {
+        sum += matrix[r][c + 1];
+    }
+    int delta = 2 * sum * matrix[r][c];
+    if (delta <= 0) {
+        matrix[r][c] = -matrix[r][c];
+    }
+    else if (delta == 4) {
+        float rnd = (rand() % 10000) / 1e4;
+        if (rnd < prob[0]) {
+            matrix[r][c] = -matrix[r][c];
+        }
+        else {
             return;
         }
-	}
-	else if (delta==8){
-        float rnd = (rand() % 10000)/1e4;
-		if (rnd < prob[1]) {
-			matrix[r][c] = -matrix[r][c];
-		}
-        else{
+    }
+    else if (delta == 8) {
+        float rnd = (rand() % 10000) / 1e4;
+        if (rnd < prob[1]) {
+            matrix[r][c] = -matrix[r][c];
+        }
+        else {
             return;
         }
-	}
+    }
 
-	energy += 2*delta*J;
-    M += 2*matrix[r][c];
+    energy += 2 * delta * J;
+    M += 2 * matrix[r][c];
 
 }
 
 void initialize_lattice(std::vector < std::vector<int> >& matrix, float& energy, int& M) {
-	int k;
-	int size = L;
+    int k;
+    int size = L;
     int sum = 0;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			k = rand() % 2;
-			if (k == 0) {
-				matrix[i][j] = -1;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            k = rand() % 2;
+            if (k == 0) {
+                matrix[i][j] = -1;
                 M -= 1;
-			}
-			else {
-				matrix[i][j] = 1;
+            }
+            else {
+                matrix[i][j] = 1;
                 M += 1;
-			}
+            }
             //energy contibute
             if (i != 0) {
-                sum += matrix[i - 1][j]*matrix[i][j]*2;
+                sum += matrix[i - 1][j] * matrix[i][j] * 2;
             }
             if (j != 0) {
-                sum += matrix[i][j-1]*matrix[i][j]*2;
+                sum += matrix[i][j - 1] * matrix[i][j] * 2;
             }
 
-            if(i == L - 1) {
-                sum += matrix[0][j]*matrix[i][j]*2; //times 2 two count also contribute where i = 0
+            if (i == L - 1) {
+                sum += matrix[0][j] * matrix[i][j] * 2; //times 2 two count also contribute where i = 0
             }
-            if(j == L - 1) {
-                sum += matrix[i][0]*matrix[i][j]*2;
+            if (j == L - 1) {
+                sum += matrix[i][0] * matrix[i][j] * 2;
             }
-            }
-		}
-    energy+= -J*sum;
-	}
+        }
+    }
+    energy += -J * sum;
+}
 
 
 
 
 
 
-int write_file(std::vector<float>& energy_vec, std::vector<float>& m, std::vector<int>& t){
-    std::ofstream myfile ("data.txt");
+int write_file(std::vector<float>& energy_vec, std::vector<float>& m, std::vector<int>& t) {
+    std::ofstream myfile("data.txt");
     if (myfile.is_open())
     {
-        for(int i = 0; i < t.size(); i ++){
-            myfile << energy_vec[i] << " "<<abs(m[i])<<' '<<t[i]<<'\n';
+        for (int i = 0; i < t.size(); i++) {
+            myfile << energy_vec[i] << " " << abs(m[i]) << ' ' << t[i] << '\n';
         }
         myfile.close();
     }
@@ -165,53 +164,64 @@ int write_file(std::vector<float>& energy_vec, std::vector<float>& m, std::vecto
 
 
 
-float simulate(float T,std::vector < std::vector<int> >& lattice, float& energy, int& M) {
+float simulate(float T, std::vector < std::vector<int> >& lattice, float& energy, int& M, std::vector<std::tuple<int, int>>& rand_vect) {
 
-	using namespace std;
+    using namespace std;
     vector<float> prob(2);
-	prob[0] = exp(-4 * J/T);
-    prob[1] = exp(-8 * J/T);
+    prob[0] = exp(-4 * J / T);
+    prob[1] = exp(-8 * J / T);
 
-	vector<float> energy_vec(1);
-    energy_vec[0] =  energy;
+    vector<float> energy_vec(1);
+    energy_vec[0] = energy;
 
     vector<float> m(1);
-    m[0] = (float)M/N;
-	vector<int> t_axis(1);
+    m[0] = (float)M / N;
+    vector<int> t_axis(1);
     t_axis[0] = 0;
-	for (int i = 1; i < IT; i++){
-		flip(lattice, prob, energy, M);
+    int i;
+    for (i = 1; i < IT; i++) {
+        flip(lattice, prob, energy, M, std::get<0>(rand_vect[i]), std::get<1>(rand_vect[i]));
         if (i % N == 0) {
             // aggiornare l'energia e la magnetizzazione ogni N step (definizione di tempo nel metropolis come da libro)
-            m.push_back((float) M / N); // magnetizzazione media per sito
+            m.push_back((float)M / N); // magnetizzazione media per sito
             energy_vec.push_back(energy);
             t_axis.push_back(i / N);
 
         }
-
-	}
+    }
     return m.back();
 }
+
+void create_rand_vect(std::vector<std::tuple<int, int>>& rand_vect_0) {
+    int i;
+    for (i = 1; i < IT; i++) {
+        rand_vect_0.push_back(std::make_tuple(rand() % L, rand() % L));
+    }
+}
+
+
 
 
 int main() {
     using namespace std;
     vector<vector<int> > lattice(L, vector<int>(L));
     float energy = 0;
-    int M =0;
-    initialize_lattice(lattice,energy,M);
+    int M = 0;
+    initialize_lattice(lattice, energy, M);
     float T = 0.1;
-    vector<float> results (1);
+    vector<float> results(1);
     results[0] = 1;
-    while(T<=3.5){
-        results.push_back(simulate(T,lattice,energy,M));
-        T += 0.1;
-        cout<<abs(results.back())<<endl;
-    }
+    std::vector<std::tuple<int, int>> rand_vect(1);
+    rand_vect[0] = make_tuple(rand()%L, rand()%L);
+    create_rand_vect(rand_vect);
 
+    while (T <= 3.5) {
+        results.push_back(simulate(T, lattice, energy, M,rand_vect));
+        T += 0.1;
+        cout << abs(results.back()) << endl;
+    }
 
 
     return 0;
 
 }
-
