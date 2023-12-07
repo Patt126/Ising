51d70451826524efc1e22fa56b46b1e44502c539
Patt126 <145294744+Patt126@users.noreply.github.com>
2023-12-07 18:07:18 +0100

    Add files via upload

---
+++ serialIsingVLIN.cpp
@@ -0,0 +1,230 @@
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include<fstream>
+#include<ctime>
+#include <chrono>
+
+
+
+#define L 100
+#define N (L*L)
+#define J 1.00
+#define IT 6*1e7 //number of iterations
+
+void print_lattice(std::vector < std::vector<int> >& matrix) {
+
+    int i, j;
+    for (i = 0; i < L; i++) {
+        std::cout << std::endl;
+        for (j = 0; j < L; j++) {
+            if (matrix[i][j] == -1) {
+                std::cout << "o" << " ";
+            } else {
+                std::cout << "x" << " ";
+
+            }
+        }
+    }
+}
+
+//if needed function to correctly evaluate energy
+float evaluate(std::vector<int>& lattice) {
+    int sum=0;
+    for (int i=0;i<N;i++){
+        //energy contibute
+        if (i >= L) { //NO FIRST ROW
+            sum += lattice[i - L]*lattice[i]*2;
+        }
+        if (i%L != 0) { //NO FIRST COLUMN
+            sum += lattice[i-1]*lattice[i]*2;
+        }
+
+        if(i >= L*(L - 1)) { //LAST ROW
+            sum += lattice[i-L*(L-1)]*lattice[i]*2; //times 2 two count also contribute where i = 0
+        }
+        if((i + 1) % L  == 0) { //LAST COLUMN
+            sum += lattice[i-(L-1)]*lattice[i]*2;
+        }
+        }
+    return -J*sum;
+    }
+
+void flip(std::vector <int> & lattice, std::vector<float>& prob,float& energy, int& M, int n) {
+
+	int sum = 0;
+
+	if (n < L) {
+		sum += lattice[n+L*(L-1)];
+	}
+	else {
+		sum += lattice[n-L];
+	}
+	if (n % L == 0) {
+		sum += lattice[n + (L - 1)];
+	}
+	else {
+		sum += lattice[n - 1];
+	}
+
+	if (n >= L*(L - 1)) {
+		sum += lattice[n - L*(L-1)];
+	}
+	else {
+		sum += lattice[n + L];
+	}
+	if ((n+1) % L == 0) {
+		sum += lattice[n - (L-1)];
+	}
+	else {
+		sum += lattice[n + 1];
+	}
+    int delta = 2*sum*lattice[n];
+	if (delta <= 0) {
+        lattice[n] = -lattice[n];
+	}
+	else if (delta == 4) {
+        float rnd = (rand() % 10000)/1e4;
+		if (rnd < prob[0] ){
+            lattice[n] = -lattice[n];
+		}
+        else{
+            return;
+        }
+	}
+	else if (delta==8){
+        float rnd = (rand() % 10000)/1e4;
+		if (rnd < prob[1]) {
+            lattice[n] = -lattice[n];
+		}
+        else{
+            return;
+        }
+	}
+
+	energy += 2*delta*J;
+    M += 2*lattice[n];
+
+}
+
+void initialize_lattice(std::vector<int>& lattice, float& energy, int& M) {
+	int k;
+	int size = N;
+    int sum = 0;
+	for (int i = 0; i < size; i++) {
+        k = rand() % 2;
+        if (k == 0) {
+            lattice[i] = -1;
+            M -= 1;
+        }
+        else {
+            lattice[i] = 1;
+            M += 1;
+			}
+            //energy contibute
+            if (i >= L) { //NO FIRST ROW
+                sum += lattice[i - L]*lattice[i]*2;
+            }
+            if (i%L != 0) { //NO FIRST COLUMN
+                sum += lattice[i-1]*lattice[i]*2;
+            }
+
+            if(i >= L*(L - 1)) { //LAST ROW
+                sum += lattice[i-L*(L-1)]*lattice[i]*2; //times 2 two count also contribute where i = 0
+            }
+            if((i + 1) % L  == 0) { //LAST COLUMN
+                sum += lattice[i-(L-1)]*lattice[i]*2;
+            }
+
+		}
+    energy+= -J*sum;
+	}
+
+
+
+
+
+
+int write_file(std::vector<float>& energy_vec, std::vector<float>& m, std::vector<int>& t){
+    std::ofstream myfile ("data.txt");
+    if (myfile.is_open())
+    {
+        for(int i = 0; i < t.size(); i ++){
+            myfile << energy_vec[i] << " "<<abs(m[i])<<' '<<t[i]<<'\n';
+        }
+        myfile.close();
+    }
+    else std::cout << "Unable to open file";
+    return 0;
+}
+
+
+
+float simulate(float T,std::vector <int> & lattice, float& energy, int& M,std::vector<int>& rand_vect) {
+
+	using namespace std;
+    vector<float> prob(2);
+	prob[0] = exp(-4 * J/T);
+    prob[1] = exp(-8 * J/T);
+
+	vector<float> energy_vec(1);
+    energy_vec[0] =  energy;
+
+    vector<float> m(1);
+    m[0] = (float)M/N;
+	vector<int> t_axis(1);
+    t_axis[0] = 0;
+    int n;
+	for (int i = 1; i < IT; i++){
+        n = rand_vect[i];
+		flip(lattice, prob, energy, M, n);
+        if (i % N == 0) {
+            // aggiornare l'energia e la magnetizzazione ogni N step (definizione di tempo nel metropolis come da libro)
+            m.push_back((float) M / N); // magnetizzazione media per sito
+            energy_vec.push_back(energy);
+            t_axis.push_back(i / N);
+
+        }
+
+	}
+    return m.back();
+}
+
+void create_rand_vect(std::vector<int>& rand_vect_0) {
+    int i;
+    for (i = 0; i < IT; i++) {
+        rand_vect_0.push_back(rand() % N );
+    }
+}
+
+
+
+
+int main() {
+    using namespace std;
+    unsigned seed = time(0);
+    srand(seed);
+    vector<int> lattice(N);
+    float energy = 0;
+    int M =0;
+    initialize_lattice(lattice,energy,M);
+    float T = 0.1;
+    vector<float> results (1);
+    results[0] = 1;
+
+    std::vector<int> rand_vect;
+    create_rand_vect(rand_vect);
+
+    while(T<=3.5){
+        results.push_back(simulate(T,lattice,energy,M,rand_vect));
+        T += 0.02;
+        cout<<abs(results.back())<<endl;
+    }
+
+
+
+    return 0;
+
+}
+
