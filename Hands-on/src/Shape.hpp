2de06672e5f93aa0805631e3538ccc156ff85851
Maria Pia Marini <80459213+piamarini99@users.noreply.github.com>
2023-12-18 19:52:27 +0100

    Update Shape.hpp

--- Hands-on/src/Shape.hpp
+++ Hands-on/src/Shape.hpp
@@ -1,40 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <random>
-
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
 
-class Shape {
-    public:
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
 
-       Shape()= default;
-
-       Shape(int nDim) : n(nDim) {}
-
-       virtual std::vector<double> generateVector() = 0;
-       virtual void calculateNorma() = 0;
-       
-
-       int getDimensions() const { return n; }
-       double getNorma () const { return norma; }
-       std::vector<double> getPoint () const { return point; }
-       std::vector<Edges> bounds () const { return bounds; }
-       const std::string getFunction() const { return function; }
-
-    protected:
-    int n{};
-    double norma{};
-    std::vector<double> point {};
-    std::string function {};
-    std::vector<Edges> bounds;
-    std::mt19937 engine;
-    
-    struct Edges {
-        double x, 
-        double y;
-    };
+struct Edges {
+  double x;
+  double y;
 };
 
-#endif 
+class Shape {
+ public:
+  Shape() = default;
+
+  Shape(int nDim) : n(nDim) {}
+
+  virtual std::vector<double> generateVector() = 0;
+  virtual void calculateNorma() = 0;
+
+  int getDimensions() const { return n; }
+  double getNorma() const { return norma; }
+  std::vector<double> getPoint() const { return point; }
+  std::vector<Edges> get_bounds() const { return bounds; }
+  const std::string getFunction() const { return function; }
+
+ protected:
+  int n{};
+  double norma{};
+  std::vector<double> point{};
+  std::string function{};
+  std::vector<Edges> bounds;
+  std::mt19937 engine;
+};
+
+#endif
