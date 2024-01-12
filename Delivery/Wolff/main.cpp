13d5ee08c35e9a90aafa840820ca6c675891a7b0
Patt126 <145294744+Patt126@users.noreply.github.com>
2024-01-12 11:48:54 +0100

    Update main.cpp

--- Delivery/Wolff/main.cpp
+++ Delivery/Wolff/main.cpp
@@ -12,21 +12,51 @@
 #include <numeric>
 #include <omp.h>
 #include <random>
-// Global vector of thread-local RNGs
 
 
-int main(){
-    int L ;
-    int L_MAX = 1024;   
+int main() {
+    int L_MIN;
+    int L_MAX;
+    float T_MIN;
+    float T_MAX;
+    float T_STEP;
+    float J;
 
+    std::cout << "Enter L_MIN: ";
+    std::cin >> L_MIN;
 
-    for(L = 64; L <= L_MAX; L *= 2){
-        int IT = 800;
-        std::cout<<"Simulation start for L = "<<L<<std::endl;
-        Wolff simulation(1.0f, L, 0.1f, 1.0f, 0.1f, IT);
+    std::cout << "Enter L_MAX: ";
+    std::cin >> L_MAX;
+
+    std::cout << "Enter T_MIN: ";
+    std::cin >> T_MIN;
+
+    std::cout << "Enter T_MAX: ";
+    std::cin >> T_MAX;
+
+    std::cout << "Enter T_STEP: ";
+    std::cin >> T_STEP;
+
+    std::cout << "Enter J: ";
+    std::cin >> J;
+
+    std::vector<float> ITVector;
+
+    float IT;
+    for (int L = L_MIN; L <= L_MAX; L *= 2) {
+        std::cout << "Enter IT for L = " << L << ": ";
+        std::cin >> IT;
+        ITVector.emplace_back(IT);
+    }
+
+    int L = L_MIN;
+    for(int i = 0; i < ITVector.size(); i++) {
+         std::cout<<"Simulation start for L = "<<L<<std::endl;
+        Wolff simulation(J, L, T_MIM, T_MAX, T_STEP, ITVector[i]);
         simulation.simulate_phase_transition();
         std::cout<<"Simulation done for L = "<<L<<std::endl;
         simulation.store_results_to_file();
+        L *= 2;
     }
     
     }
